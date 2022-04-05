a6bb001c7cd1eb2490bd7cb24e6e717684b482e8
bkmanish01 <hmcmanish01@gmail.com>
2022-04-04 19:23:20 -0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'antd/dist/antd.css';
+
+
+function App() {
+  return (
+    <div className='App'>
+     <h1>Hello, this is my first react app!</h1>
+      <h2>What is Git?</h2>
+      <p>Programmer track code </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h1 style={{textAlign:'center'}}>Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
